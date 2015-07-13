a915892af86570fd2904786c54243e92d564273d
Indra Prasetya <punyaindra@gmail.com>
2015-07-13 15:31:00 +0700

    remove closure

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,23 +20,23 @@ Route::get('/home', 'PostController@index');
 Route::get('/post/{slug}', 'PostController@show');
 Route::resource("posts","PostController");
 
-Route::get('/sitemap.xml', function(){
-	$sitemap = App::make("sitemap" );
-
-	$sitemap->add(URL::to('/'), '2013-11-16T12:30:00+02:00', '1.0', 'daily');
-	$sitemap->add(URL::to('about'), '2013-11-16T12:30:00+02:00', '0.7', 'monthly');
-	$sitemap->add(URL::to('life'), '2013-11-16T12:30:00+02:00', '0.7', 'monthly');
-	$sitemap->add(URL::to('code'), '2013-11-16T12:30:00+02:00', '0.7', 'monthly');
-
-	$posts = App\Post::all();
-
-	foreach($posts as $post) 
-	{
-		$sitemap->add(URL::to("post/{$post->slug}"), $post->created_at, '0.9', 'weekly');
-	}
-
-	return $sitemap->render('xml');
-});
+//Route::get('/sitemap.xml', function(){
+//	$sitemap = App::make("sitemap" );
+//
+//	$sitemap->add(URL::to('/'), '2013-11-16T12:30:00+02:00', '1.0', 'daily');
+//	$sitemap->add(URL::to('about'), '2013-11-16T12:30:00+02:00', '0.7', 'monthly');
+//	$sitemap->add(URL::to('life'), '2013-11-16T12:30:00+02:00', '0.7', 'monthly');
+//	$sitemap->add(URL::to('code'), '2013-11-16T12:30:00+02:00', '0.7', 'monthly');
+//
+//	$posts = App\Post::all();
+//
+//	foreach($posts as $post) 
+//	{
+//		$sitemap->add(URL::to("post/{$post->slug}"), $post->created_at, '0.9', 'weekly');
+//	}
+//
+//	return $sitemap->render('xml');
+//});
 
 Route::controllers([
 	'auth' => 'Auth\AuthController',
