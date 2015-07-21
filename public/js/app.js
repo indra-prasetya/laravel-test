137610b7d0d063378a1ef8c32076602a634f4de4
Indra <punyaindra@gmail.com>
2015-07-21 16:03:56 +0700

    add transition animation

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 jQuery(function($) {
-  $(document).on('page:fetch',   function() { NProgress.start();  });
+  $(document).on('page:fetch',   function() { NProgress.start(); $('.site-heading, #content').addClass('animated fadeOutUp'); });
   $(document).on('page:receive', function() { NProgress.set(0.5); });
-  $(document).on('page:change',  function() { NProgress.done();   });
+  $(document).on('page:change',  function() { NProgress.done(); $('.site-heading, #content').addClass('animated fadeInDown');  });
   $(document).on('page:restore', function() { NProgress.remove(); });
   
   $('[data-toggle="tooltip"]').tooltip();
