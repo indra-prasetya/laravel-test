c73c614501bc8297d389ecdd454a8a9603954570
Indra <punyaindra@gmail.com>
2015-07-23 14:47:50 +0700

    add fadein effect, norak yes!!

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,28 @@
-jQuery(function($) {
-  $(document).on('page:fetch',   function() { NProgress.start(); $('.site-heading, #content').addClass('animated fadeOutUp'); });
-  $(document).on('page:receive', function() { NProgress.set(0.5); });
-  $(document).on('page:change',  function() { NProgress.done(); $('.site-heading, #content').addClass('animated fadeInDown');  });
-  $(document).on('page:restore', function() { NProgress.remove(); });
-  
-  $('[data-toggle="tooltip"]').tooltip();
+jQuery(function ($) {
+    $(document).on('page:fetch', function () {
+        NProgress.start();
+        $('.site-heading').addClass('animated fadeOutUp');
+        $('#content').addClass('animated fadeOut');
+    });
+    $(document).on('page:receive', function () {
+        NProgress.set(0.5);
+    });
+    $(document).on('page:change', function () {
+        NProgress.done();
+        $('.site-heading').addClass('animated fadeInDown');
+        $('#content').addClass('animated fadeIn');
+    });
+    $(document).on('page:restore', function () {
+        NProgress.remove();
+    });
+
+    $('[data-toggle="tooltip"]').tooltip();
 });
 
 NProgress.configure({
-  showSpinner: false,
-  ease: 'ease',
-  speed: 1500
+    showSpinner: false,
+    ease: 'ease',
+    speed: 1500
 });
 
 
