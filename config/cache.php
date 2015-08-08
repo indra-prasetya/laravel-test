1b80a5c3db35dec40764430a08b544555f5ffc30
Indra Prasetya <punyaindra@gmail.com>
2015-08-08 15:47:49 +0700

    set apc

--- config/cache.php
+++ config/cache.php
@@ -20,7 +20,7 @@ return [
 	|
 	*/
 
-	'default' => env('CACHE_DRIVER', 'file'),
+	'default' => env('CACHE_DRIVER', 'apc'),
 
 	/*
 	|--------------------------------------------------------------------------
