9565e50a3d2a196e38b13c1a9ec47a001bf81d14
Indra Prasetya <punyaindra@gmail.com>
2015-08-16 22:04:30 +0700

    change this and that

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,6 +28,6 @@ Route::resource("posts","PostController");
 Route::get('/sitemap.xml',"SitemapController@index")->before('cache.fetch')->after('cache.put');
 
 Route::controllers([
-	'manage' => 'Auth\AuthController',
+	'auth' => 'Auth\AuthController',
 	//'password' => 'Auth\PasswordController',
 ]);
