0f8f55d52d2951f55041119c3b3f8cc6b69c8cdb
Indra Prasetya <punyaindra@gmail.com>
2015-09-21 15:11:45 +0700

    add forever cached

--- app/Filters/CacheFilter.php
+++ app/Filters/CacheFilter.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Filters;
 
 use Cache;
@@ -7,24 +8,28 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Str;
 
-class CacheFilter {
-	
-	public function fetch (Route $route, Request $request)
-	{
-		$key = $this->_makeCacheKey($request->url());
-		
-		if(Cache::has($key)) return Cache::get($key);
-	}
-	
-	public function put (Route $route, Request $request, Response $response)
-	{
-		$key = $this->_makeCacheKey($request->url());
-		
-		if(!Cache::has($key)) return Cache::put($key, $response->getContent(), 3600);
-	}
+class CacheFilter
+{
 
-	private function _makeCacheKey($url)
-	{
-		return 'route_' . Str::slug($url);
-	}
+    public function fetch(Route $route, Request $request)
+    {
+        $key = $this->_makeCacheKey($request->url());
+
+        if (Cache::has($key))
+            return Cache::get($key);
+    }
+
+    public function put(Route $route, Request $request, Response $response)
+    {
+        $key = $this->_makeCacheKey($request->url());
+
+        if (!Cache::has($key))
+            return Cache::forever($key, $response->getContent());
+    }
+
+    private function _makeCacheKey($url)
+    {
+        return 'route_' . Str::slug($url);
+    }
+
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,15 +1,18 @@
-<?php namespace App\Http\Controllers;
+<?php
+
+namespace App\Http\Controllers;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
-
+use Cache;
 use App\Post;
 use App\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
-class PostController extends Controller {
-	
+class PostController extends Controller
+{
+
     /**
      * Create a new controller instance.
      *
@@ -17,158 +20,165 @@ class PostController extends Controller {
      */
     public function __construct()
     {
-        $this->middleware('auth', ['except' => ['index','life','code','about','show', 'sitemap']]);
+        $this->middleware('auth', ['except' => ['index', 'life', 'code', 'about', 'show', 'sitemap']]);
     }
-	
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		$posts = Post::where('is_published', '=', true)->orderBy('created_at', 'DESC')->simplePaginate(5);
-		return view('posts.index', compact('posts'));
-	}
-	public function life()
-	{
-		$posts = Post::whereRaw('is_published = 1 AND category_id = 1')->orderBy('created_at', 'DESC')->simplePaginate(5);
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $posts = Post::where('is_published', '=', true)->orderBy('created_at', 'DESC')->simplePaginate(5);
+        return view('posts.index', compact('posts'));
+    }
+
+    public function life()
+    {
+        $posts = Post::whereRaw('is_published = 1 AND category_id = 1')->orderBy('created_at', 'DESC')->simplePaginate(5);
         $title = 'indra.prasetya - life';
-		return view('posts.index', compact('posts', 'title'));
-	}
-	public function code()
-	{
-		$posts = Post::whereRaw('is_published = 1 AND category_id = 2')->orderBy('created_at', 'DESC')->simplePaginate(5);
+        return view('posts.index', compact('posts', 'title'));
+    }
+
+    public function code()
+    {
+        $posts = Post::whereRaw('is_published = 1 AND category_id = 2')->orderBy('created_at', 'DESC')->simplePaginate(5);
         $title = 'indra.prasetya - code';
-		return view('posts.index', compact('posts', 'title'));
-	}
-	public function about()
-	{
-		$posts = Post::whereRaw('is_published = 1 AND category_id = 3')->orderBy('created_at', 'DESC')->simplePaginate(5);
+        return view('posts.index', compact('posts', 'title'));
+    }
+
+    public function about()
+    {
+        $posts = Post::whereRaw('is_published = 1 AND category_id = 3')->orderBy('created_at', 'DESC')->simplePaginate(5);
         $title = 'indra.prasetya - about';
-		return view('posts.index', compact('posts', 'title'));
-	}
+        return view('posts.index', compact('posts', 'title'));
+    }
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		$categories = Category::where('is_active', '=', true)->get();
-		return view('posts.create', compact('categories'));
-	}
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        $categories = Category::where('is_active', '=', true)->get();
+        return view('posts.create', compact('categories'));
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @param Request $request
-	 * @return Response
-	 */
-	public function store(Request $request)
-	{
-		$post = new Post();
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        $post = new Post();
 
-		$post->title = $request->input("title");
-                $post->category_id = $request->input("category_id");
-                $post->slug = $request->input("slug");
-                $post->description = $request->input("description");
-                $post->content = $request->input("content");
-                $post->is_published = $request->input("is_published");
+        $post->title = $request->input("title");
+        $post->category_id = $request->input("category_id");
+        $post->slug = $request->input("slug");
+        $post->description = $request->input("description");
+        $post->content = $request->input("content");
+        $post->is_published = $request->input("is_published");
 
-		$post->save();
+        $post->save();
+        Cache::flush();
 
-		return redirect()->route('posts.index')->with('message', 'Item created successfully.');
-	}
+        return redirect()->route('posts.index')->with('message', 'Item created successfully.');
+    }
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($slug)
-	{
-		$post = Post::where('slug','=',$slug)->firstOrFail();		
-		$is_post = true;
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($slug)
+    {
+        $post = Post::where('slug', '=', $slug)->firstOrFail();
+        $is_post = true;
 
-		return view('posts.show', compact('post', 'is_post'));
-	}
+        return view('posts.show', compact('post', 'is_post'));
+    }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		$post = Post::findOrFail($id);
-		$is_post = true;
-		$categories = Category::where('is_active', '=', true)->get();
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        $post = Post::findOrFail($id);
+        $is_post = true;
+        $categories = Category::where('is_active', '=', true)->get();
 
-		return view('posts.edit', compact('post', 'categories'));
-	}
+        return view('posts.edit', compact('post', 'categories'));
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @param Request $request
-	 * @return Response
-	 */
-	public function update(Request $request, $id)
-	{
-		$post = Post::findOrFail($id);
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @param Request $request
+     * @return Response
+     */
+    public function update(Request $request, $id)
+    {
+        $post = Post::findOrFail($id);
 
-		$post->title = $request->input("title");
-                $post->category_id = $request->input("category_id");
-                $post->slug = $request->input("slug");
-                $post->description = $request->input("description");
-                $post->content = $request->input("content");
-                $post->is_published = $request->input("is_published");
+        $post->title = $request->input("title");
+        $post->category_id = $request->input("category_id");
+        $post->slug = $request->input("slug");
+        $post->description = $request->input("description");
+        $post->content = $request->input("content");
+        $post->is_published = $request->input("is_published");
 
-		$post->save();
+        $post->save();
+        Cache::flush();
 
-		return redirect()->route('posts.index')->with('message', 'Item updated successfully.');
-	}
+        return redirect()->route('posts.index')->with('message', 'Item updated successfully.');
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		$post = Post::findOrFail($id);
-		$post->delete();
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+        Cache::flush();
 
-		return redirect()->route('posts.index')->with('message', 'Item deleted successfully.');
-	}
+        return redirect()->route('posts.index')->with('message', 'Item deleted successfully.');
+    }
 
-	public function sitemap()
-	{
-		$sitemap = App::make("sitemap");
+    public function sitemap()
+    {
+        $sitemap = App::make("sitemap");
 
-		$sitemap->add(URL::to('/'), '2013-11-16T12:30:00+02:00', '1.0', 'daily');
-		$sitemap->add(URL::to('about'), '2013-11-16T12:30:00+02:00', '0.7', 'monthly');
-		$sitemap->add(URL::to('life'), '2013-11-16T12:30:00+02:00', '0.7', 'monthly');
-		$sitemap->add(URL::to('code'), '2013-11-16T12:30:00+02:00', '0.7', 'monthly');		
-		
-		$posts = Post::all();
-		
-		foreach($posts as $post) {
-		  $sitemap->add(URL::to("post/{$post->slug}"), $post->created_at, '0.9', 'weekly');
-		}
-		
-		return $sitemap->render('xml');
-	}
-	
-	public function info()
-	{
-		phpinfo();
-		exit();
-	}
+        $sitemap->add(URL::to('/'), '2013-11-16T12:30:00+02:00', '1.0', 'daily');
+        $sitemap->add(URL::to('about'), '2013-11-16T12:30:00+02:00', '0.7', 'monthly');
+        $sitemap->add(URL::to('life'), '2013-11-16T12:30:00+02:00', '0.7', 'monthly');
+        $sitemap->add(URL::to('code'), '2013-11-16T12:30:00+02:00', '0.7', 'monthly');
+
+        $posts = Post::all();
+
+        foreach ($posts as $post) {
+            $sitemap->add(URL::to("post/{$post->slug}"), $post->created_at, '0.9', 'weekly');
+        }
+
+        return $sitemap->render('xml');
+    }
+
+    public function info()
+    {
+        phpinfo();
+        exit();
+    }
+
 }
