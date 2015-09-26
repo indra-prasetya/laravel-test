e958f0fad865994cda35e1184dc536f230078e78
Indra Prasetya <punyaindra@gmail.com>
2015-09-26 08:16:17 +0700

    fix scroll top

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,6 +15,27 @@ jQuery(function ($) {
 
     $('[data-toggle="tooltip"]').tooltip();
     
+    if ($(window).width() > 1170) {
+        var headerHeight = $('.navbar-custom').height();
+        var lastScrollTop = 0;
+        $(window).on('scroll', 
+            function() {
+                var currentTop = $(this).scrollTop();
+                //check if user is scrolling up
+                if (currentTop < lastScrollTop) {
+                    if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+                        $('.navbar-custom').addClass('is-visible');
+                    } else {
+                        $('.navbar-custom').removeClass('is-visible is-fixed');
+                    }
+                } else {
+                    //if scrolling down...
+                    $('.navbar-custom').removeClass('is-visible');
+                    if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+                }
+                lastScrollTop = currentTop;
+            });
+    }
 });
 
 NProgress.configure({
