34f340756649e1668281cb060f00487f84ccb208
Indra Prasetya <punyaindra@gmail.com>
2016-03-22 16:27:21 +0700

    set https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider {
 	 */
 	public function boot()
 	{
-		//
+            if (!\App::environment('local')) {
+                \URL::forceSchema('https');
+            }
 	}
 
 	/**
